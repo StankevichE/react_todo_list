ebb3134eba16b71a2b6926b43d45e8d5bd98fb46
StankevichE <119804874+StankevichE@users.noreply.github.com>
2023-03-12 18:28:13 +0300

    edit commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Header from "./components/Header";
+import Form from "./components/Form";
+import React, {useState} from "react";
+import TodosList from "./components/TodosList";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+    const [input, setInput] = useState('')
+    const [todos, setTodos] = useState([])
+    const [editTodo, setEditTodo] = useState(null)
+
+    return (
+        <div className="container">
+            <div className="app-wrapper">
+
+                <div className="App">
+                    <Header />
+                    </div>
+
+                        <div>
+                            <Form
+                                input={input}
+                                setInput={setInput}
+                                todos={todos}
+                                setTodos={setTodos}
+                                editTodo={editTodo}
+                                setEditTodo={setEditTodo}
+                            />
+                        </div>
+                            <div>
+                                <TodosList
+                                todos={todos}
+                                setTodos={setTodos}
+                                setEditTodo={setEditTodo}
+                                />
+                            </div>
+                    </div>
+                </div>
+         );
+                }
+ export default App;
